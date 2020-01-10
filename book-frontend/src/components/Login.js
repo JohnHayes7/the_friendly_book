f5ec6866a8f7aed6301d4ca2c1aac3c917658cf4
John Hayes <JohnnyHayes7@gmail.com>
2020-01-10 13:51:02 -0500

    added login form

--- book-frontend/src/components/Login.js
+++ book-frontend/src/components/Login.js
@@ -4,8 +4,16 @@ import './landing_page.css'
 export default class Login extends React.Component{
     render(){
         return(
-            <div>
-                LogIn
+            <div id="login-form">
+                <span id='login-label'>LogIn</span>
+                <form>
+                    <label>UserName:</label>
+                    <input type="text"/><br></br>
+                    <label>Password:</label>
+                    <input type="text"/><br></br>
+                    <input type="submit"/>
+                    
+                </form>
             </div>
         )
     }
