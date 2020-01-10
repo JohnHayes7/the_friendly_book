6011c48420ef40515b76f2377752af82b0bf732b
John Hayes <JohnnyHayes7@gmail.com>
2020-01-10 16:37:20 -0500

    successfully added videos to landing page

--- book-frontend/src/components/LandingMain.js
+++ book-frontend/src/components/LandingMain.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import Youtube from 'react-youtube'
 
 export default class LandingMain extends React.Component{
     render(){
+
+        const opts = {
+            height: '200',
+            width: '410'
+        }
         return(
             <div id="landingPage-main">
                 Random Phish Video
                 <div id="landingMain-Vids">
                     <div className="landingPageVid" id="land-vid-1">
-                        Video 1
+                        <Youtube videoId={this.props.randomVideos[0]} opts={opts} />
                     </div>
                     <div className="landingPageVid" id="land-vid-2">
-                        Video 2
+                        <Youtube videoId={this.props.randomVideos[3]} opts={opts} />
                     </div>
                 </div>
             </div>
