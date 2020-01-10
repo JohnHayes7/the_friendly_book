a42f0ff37247c5ecf1ef08d73d639b85e366fb9b
John Hayes <JohnnyHayes7@gmail.com>
2020-01-10 16:25:37 -0500

    can successfully retrieve YT Videos

--- book-frontend/src/containers/LandingMain_Container.js
+++ book-frontend/src/containers/LandingMain_Container.js
@@ -2,13 +2,42 @@ import React from 'react'
 import LandingMain from '../components/LandingMain'
 
 class LandingMainContainer extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            randomVideos: []
+        }
+    }
+
+    getVideos = () => {
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=phish&key=${YT_APIKEY}`).then(response => response.json())
+        .then(videos => {
+            videos.items.shift()
+            videos.items.map(video => {
+                return this.setState(prevState => ({
+                    randomVideos: [...prevState.randomVideos, video.id.videoId ]
+                }))
+            })
+           
+        })
+    }
+    
+
+    componentDidMount(){
+        this.getVideos()
+    }
+
     render(){
         return(
             <div id="landingMainContainer">
-                <LandingMain />
+                <LandingMain randomVideos={this.state.randomVideos}/>
             </div>
         )
     }
 }
+
+const YT_APIKEY = process.env.REACT_APP_YOUTUBE_API_KEY
+
 
 export default LandingMainContainer
