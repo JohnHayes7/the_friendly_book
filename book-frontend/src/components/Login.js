a36de88aca5489ca48716a31a66a2819a45e6df8
John Hayes <JohnnyHayes7@gmail.com>
2020-01-10 11:00:10 -0500

    added login and login_controller components

--- book-frontend/src/components/Login.js
+++ book-frontend/src/components/Login.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import LogInController from '../controllers/Log_In_Controller'
 
 export default class Login extends React.Component{
     render(){
         return(
             <div>
-                <h1>The Friendly Book</h1>
-                <LogInController />
+                <h3>LogIn</h3>
             </div>
         )
     }
---
+++ book-frontend/src/controllers/Log_In_Controller.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from '../components/Login'
+
+class LogInController extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Log_in_Controller</h2>
+                <Login />
+            </div>
+        )
+    }
+}
+
+export default LogInController
