79a49386cc6670a324c5413cd6aa9df44d4838ef
John Hayes <JohnnyHayes7@gmail.com>
2020-01-14 12:12:39 -0500

    setting up redux for login

--- book-frontend/src/components/Login.js
+++ book-frontend/src/components/Login.js
@@ -2,15 +2,38 @@ import React from 'react'
 import './landing_page.css'
 
 export default class Login extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            username: "",
+            password:""
+        }
+    }
+
+    usernameChangeHandler = event => {
+        console.log(event.target.value)
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    passwordChangeHandler = event => {
+        console.log(event.target.value)
+        this.setState({
+            password: event.target.value
+        })
+    }
+
     render(){
         return(
             <div id="login-form">
                 <span id='login-label'>Log In</span>
                 <form>
                     <label>UserName:</label>
-                    <input type="text"/><br></br>
+                    <input type="text" value={this.state.username} onChange={event => this.usernameChangeHandler(event)}/><br></br>
                     <label>Password:</label>
-                    <input type="text"/><br></br>
+                    <input type="text" value={this.state.password} onChange={event => this.passwordChangeHandler(event)}/><br></br>
                     <input type="submit"/>
                 </form><br></br>
                 <a href="#" >Create a profile</a>
--- book-frontend/src/containers/Log_In_Container.js
+++ book-frontend/src/containers/Log_In_Container.js
@@ -3,6 +3,15 @@ import Login from '../components/Login'
 import '../components/landing_page.css'
 
 class LogInController extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            name: "",
+            password:""
+        }
+    }
+
     render(){
         return(
             <div id="login-window">
