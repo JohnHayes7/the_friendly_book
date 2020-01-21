89c7652b75a2f9f49a98914f5524927397af7c1a
John Hayes <JohnnyHayes7@gmail.com>
2020-01-20 22:23:37 -0500

    can successfully retrieve sets from shows and set state

--- book-frontend/src/containers/YearPage_Container.js
+++ book-frontend/src/containers/YearPage_Container.js
@@ -18,15 +18,39 @@ export default class YearPageContainer extends React.Component{
         fetch(`https://api.relisten.net/api/v2/artists/phish/years/${this.props.match.params.year}`).then(response => response.json())
         .then(shows => {
             let yearShowsAry = []
+            
            shows.shows.map(show => {
+            let setOne = []
+            let setTwo = []
+            let encore = []
+               fetch(`https://api.relisten.net/api/v2/artists/phish/shows/${show.display_date}`).then(response => response.json())
+               .then(showSets => {
+                    let firstSet = showSets.sources[0].sets[0].tracks
+                    firstSet.map(songTitle => {
+                        setOne.push(songTitle.title)
+                    })
+                    
+                    if(showSets.sources[0].sets[1]){
+                        showSets.sources[0].sets[1].tracks.map(song => {
+                            setTwo.push(song.title)
+                        })
+                    }
+                    
+                    if(showSets.sources[0].sets[2]){
+                        showSets.sources[0].sets[2].tracks.map(song => {
+                            encore.push(song.title)
+                        })
+                    }
+
+               })
                 const newShow = {
                     date: show.display_date,
                     location: show.venue.location,
                     venue: show.venue.name,
                     setlist:{
-                        set1: [],
-                        set2: [],
-                        encore: []
+                        set1: setOne,
+                        set2: setTwo,
+                        encore: encore
                     }
                 }
                 yearShowsAry = [...yearShowsAry, newShow]
@@ -36,7 +60,7 @@ export default class YearPageContainer extends React.Component{
                 shows: yearShowsAry
             })
            console.log(this.state)
-           this.getSetsFromYearShows()
+          
         })
     }
 
@@ -57,40 +81,40 @@ export default class YearPageContainer extends React.Component{
     }
       
 
-    getSetsFromYearShows = () =>{
-        this.state.shows.map(show => {
-            fetch(`https://api.relisten.net/api/v2/artists/phish/shows/${show.date}`).then(response => response.json())
-            .then(rxShow => {
-                debugger
-                // const setOneTitles = []
-                // const setTwoTitles = []
-                // const encoreTitles = []
+    // getSetsFromYearShows = () =>{
+    //     this.state.shows.map(show => {
+    //         fetch(`https://api.relisten.net/api/v2/artists/phish/shows/${show.date}`).then(response => response.json())
+    //         .then(rxShow => {
                 
-                // let setOne = rxShow.sources[0].sets[0].tracks
-                // let setTwo = rxShow.sources[0].sets[1].tracks
-                // let encore = rxShow.sources[0].sets[2].tracks
+    //             const setOneTitles = []
+    //             const setTwoTitles = []
+    //             const encoreTitles = []
                 
-                // setOne.map( track => {
-                //     setOneTitles.push(track.title)
-                // })
+    //             let setOne = rxShow.sources[0].sets[0].tracks
+    //             let setTwo = rxShow.sources[0].sets[1].tracks
+    //             let encore = rxShow.sources[0].sets[2].tracks
+                
+    //             setOne.map( track => {
+    //                 setOneTitles.push(track.title)
+    //             })
 
-                // if(setTwo){
-                //     setTwo.map( track => {
-                //         setTwoTitles.push(track.title)
-                //     })
-                // }
+    //             if(setTwo){
+    //                 setTwo.map( track => {
+    //                     setTwoTitles.push(track.title)
+    //                 })
+    //             }
 
-                // if(encore){
-                //     encore.map( track => {
-                //         encoreTitles.push(track.title)
-                //     })
-                // }
+    //             if(encore){
+    //                 encore.map( track => {
+    //                     encoreTitles.push(track.title)
+    //                 })
+    //             }
                 
-            })
+    //         })
             
-        })
+    //     })
         
-    }
+    // }
 
     componentDidMount() {
         this.getShowsFromYear()
