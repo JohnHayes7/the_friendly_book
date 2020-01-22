b2d80e8824589b6469d937224456dc3bee0e25eb
John Hayes <JohnnyHayes7@gmail.com>
2020-01-21 21:56:09 -0500

    persisting show_dates to DB

--- book-backend/app/controllers/shows_dates_controller.rb
+++ book-backend/app/controllers/shows_dates_controller.rb
@@ -1,20 +1,26 @@
 class ShowsDatesController < ApplicationController
 
     def create
-        
+       
         params[:_json].each do |s|
+
             day = ShowDate.get_day(s[:date])
             month = ShowDate.get_month(s[:date])
             show_year = ShowDate.get_year(s[:date])
+
+            
 
             year = Year.find_by(value: show_year)
 
             show_date = ShowDate.new({month: month, day: day})
             show_date.year_id= year.id
-            show_date.venue_id = 0
+            show_date.venue_id = 2
             show_date.save
-            binding.pry
         end
+        show_dates = ShowDate.all
+        options = {include: [:show, :year, :venue]}
+        render json: ShowDateSerializer.new(show_dates, options)
+        
     end
     
     def index
