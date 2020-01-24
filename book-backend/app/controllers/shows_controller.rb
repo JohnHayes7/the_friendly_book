23a12a2f2b901d0a1010296ac634a008abdddf65
John Hayes <JohnnyHayes7@gmail.com>
2020-01-23 21:04:55 -0500

    wiring up creating shows

--- book-backend/app/controllers/shows_controller.rb
+++ book-backend/app/controllers/shows_controller.rb
@@ -2,6 +2,24 @@ class ShowsController < ApplicationController
 
 
     def create
+        day = ShowDate.get_day(params[:date])
+        month = ShowDate.get_month(params[:date])
+
+        show_date = ShowDate.find_by({month: month, day:day})
+        venue = Venue.find_by(name: params[:venue])
+        if show_date.show == nil
+            show = Show.new
+            set1 = ""
+            params[:set1].each do |s|
+                set1 += "#{s}, "
+            end
+            show.set1 = set1 
+            if venue
+                show.venue_id = venue.id
+                show.save
+            end
+        end
+
         binding.pry
     end
 
