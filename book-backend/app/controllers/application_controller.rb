4f044e430ecb84740dbcb8d28545815a42c25311
John Hayes <JohnnyHayes7@gmail.com>
2020-01-29 16:30:33 -0500

    added Knock and JWT gem, continuing debugging auth and login issues

--- book-backend/app/controllers/application_controller.rb
+++ book-backend/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
     include ::ActionController::Cookies
-    # protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
     # skip_before_action :verify_authenticity_token
     
     def login!
