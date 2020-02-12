7c161723acadc7c64235330f23285904b105e9ed
John Hayes <JohnnyHayes7@gmail.com>
2020-02-12 18:00:21 -0500

    had to reinstall a number of packages and dependecies.  Up and running again

--- book-frontend/src/index.js
+++ book-frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import manageFan from './reducers/manageFan'
 
